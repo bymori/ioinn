4b4eaefc5d1af61110a18d5b1837b94f3fee7b06
by_mori <by_mori@163.com>
2021-10-26 21:32:58 +0800

    CODING_API3

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
  # @Author: by_mori
  # @Date: 2021-10-24 22:17:05
  # @LastEditors: by_mori
- # @LastEditTime: 2021-10-26 21:26:37
+ # @LastEditTime: 2021-10-26 21:31:21
 ### 
 
 # 确保脚本抛出遇到的错误
@@ -39,7 +39,8 @@ echo 'blog.ioinn.cn' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:bymori/website/ioinn.git
+  # codingUrl=git@e.coding.net:bymori/website/ioinn.git
+  codingUrl=https://dPXPSdUzQa:${CODING_TOKEN}git@e.coding.net:bymori/website/ioinn.git
 else
   #注意！！！这里需要使用coding提供的个人令牌的用户名和token
   codingUrl=https://dPXPSdUzQa:${CODING_TOKEN}git@e.coding.net:bymori/website/ioinn.git
