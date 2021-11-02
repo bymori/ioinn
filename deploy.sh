286259441209d71519144f610bc3b03cc90e2fd7
by_mori <by_mori@163.com>
2021-11-02 22:35:53 +0800

    coding

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
  # @Author: by_mori
  # @Date: 2021-10-24 22:17:05
  # @LastEditors: by_mori
- # @LastEditTime: 2021-11-02 22:19:11
+ # @LastEditTime: 2021-11-02 22:34:52
 ### 
 
 # 确保脚本抛出遇到的错误
@@ -35,9 +35,10 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding
-echo 'blog.ioinn.cn\cn' > CNAME  # 自定义域名
+# echo 'blog.ioinn.cn\cn' > CNAME  # 自定义域名
 if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:bymori/website/ioinn.git
+  # codingUrl=git@e.coding.net:bymori/website/ioinn.git
+  codingUrl=https://dPXPSdUzQa:${CODING_TOKEN}@e.coding.net:bymori/website/ioinn.git
 else
   #注意！！！这里需要使用coding提供的个人令牌的用户名和token
   codingUrl=https://dPXPSdUzQa:${CODING_TOKEN}@e.coding.net:bymori/website/ioinn.git
